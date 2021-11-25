f8a9c5758a4155b237b3ed206041974c7a6ff607
davidyang2149 <davidyang2149@gmail.com>
2021-11-25 18:40:50 +0900

    :white_check_mark: api hello 테스트 케이스를 추가합니다
    nextjs-starter

---
+++ __tests__/api/utils.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { IncomingMessage, ServerResponse } from 'http';
+import { apiResolver } from 'next/dist/server/api-utils';
+
+interface Props {
+  query?: unknown;
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
+}
+
+export default function requestListener({ query = {}, handler }: Props) {
+  return (req: IncomingMessage, res: ServerResponse) => {
+    apiResolver(
+      req,
+      res,
+      query,
+      handler,
+      {
+        previewModeId: 'id',
+        previewModeEncryptionKey: 'key',
+        previewModeSigningKey: 'key',
+      },
+      false,
+    );
+  };
+}
