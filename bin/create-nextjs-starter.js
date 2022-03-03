e46c0ac9fbe8aec8c6d6cfd6c24bf9ad05a35329
davidyang2149 <davidyang2149@gmail.com>
2022-03-03 22:09:23 +0900

    :loud_sound: CLI 문구를 명확하게 작성합니다
    update CLI comment

--- bin/create-nextjs-starter.js
+++ bin/create-nextjs-starter.js
@@ -5,25 +5,15 @@ const path = require("path");
 const fs = require("fs");
 
 if (process.argv.length < 3) {
-  console.log("Using npm.");
+  console.log("\x1b[31m%s\x1b[0m", "========================= 🚨 E R R O R 🚨 =========================");
+  console.log("\x1b[31m%s\x1b[0m", "🚨 Warning:");
   console.log("");
-  console.log("Installing dependencies:");
-  console.log("\x1b[32m%s\x1b[0m", "- react");
-  console.log("\x1b[32m%s\x1b[0m", "- react-dom");
-  console.log("\x1b[32m%s\x1b[0m", "- next");
+  console.log("Please specify the project directory:");
+  console.log("\x1b[32m%s\x1b[0m", "@davidyang2149/nextjs-starter <project-directory>");
   console.log("");
-  console.log("Installing devDependencies:");
-  console.log("\x1b[32m%s\x1b[0m", "- eslint");
-  console.log("\x1b[32m%s\x1b[0m", "- jest");
-  console.log("\x1b[32m%s\x1b[0m", "- cypress");
-  console.log("\x1b[32m%s\x1b[0m", "- typescript");
-  console.log("");
-  console.log("\x1b[36m%s\x1b[0m", "Successfully installed!");
-  console.log("");
-  console.log("⭐ Next.js Starter Kit ⭐");
-  console.log("🥰 Create by davidyang2149");
-  console.log("🚀 From https://github.com/DavidYang2149/nextjs-starter");
-  console.log("");
+  console.log("For example:");
+  console.log("npx @davidyang2149/nextjs-starter my-nextjs-project");
+  console.log("\x1b[31m%s\x1b[0m", "=========================== 🚨 E N D 🚨 ===========================");
 
   process.exit(1);
 }
@@ -38,12 +28,16 @@ if (projectName !== ".") {
     fs.mkdirSync(projectPath);
   } catch (err) {
     if (err.code === "EEXIST") {
+      console.log("\x1b[31m%s\x1b[0m", "========================= 🚨 E R R O R 🚨 =========================");
       console.log(projectName);
       console.log("\x1b[31m%s\x1b[0m",
         `🚨 The folder ${projectName} already exist in the current directory, please give it another name.`
       );
+      console.log("\x1b[31m%s\x1b[0m", "=========================== 🚨 E N D 🚨 ===========================");
     } else {
+      console.log("\x1b[31m%s\x1b[0m", "========================= 🚨 E R R O R 🚨 =========================");
       console.log(error);
+      console.log("\x1b[31m%s\x1b[0m", "=========================== 🚨 E N D 🚨 ===========================");
     }
     process.exit(1);
   }
@@ -52,6 +46,24 @@ if (projectName !== ".") {
 async function main() {
   try {
     console.log("========================= 🚀 S T A R T 🚀 =========================");
+    console.log("Using npm.");
+    console.log("");
+    console.log("Installing dependencies:");
+    console.log("\x1b[32m%s\x1b[0m", "- react");
+    console.log("\x1b[32m%s\x1b[0m", "- react-dom");
+    console.log("\x1b[32m%s\x1b[0m", "- next");
+    console.log("");
+    console.log("Installing devDependencies:");
+    console.log("\x1b[32m%s\x1b[0m", "- typescript");
+    console.log("\x1b[32m%s\x1b[0m", "- eslint");
+    console.log("\x1b[32m%s\x1b[0m", "- jest");
+    console.log("\x1b[32m%s\x1b[0m", "- testing-library");
+    console.log("\x1b[32m%s\x1b[0m", "- cypress");
+    console.log("");
+    console.log("⭐ Next.js Starter Kit ⭐");
+    console.log("🥰 Create by davidyang2149");
+    console.log("🚀 From https://github.com/DavidYang2149/nextjs-starter");
+    console.log("");
     console.log(`🚀 Creating project ${projectName}...`);
     console.log("");
     console.log("🚚 Downloading files:");
@@ -68,10 +80,14 @@ async function main() {
     console.log("🔥 Removing useless files:");
     execSync("npx rimraf ./.git");
     console.log("");
+    console.log("\x1b[36m%s\x1b[0m", "Successfully installed!");
+    console.log("");
     console.log("\x1b[35m%s\x1b[0m", "🎉 The installation is done, ready to use. Happy coding!");
     console.log("========================= 🎉 E N D 🎉 =========================");
   } catch (error) {
+    console.log("\x1b[31m%s\x1b[0m", "========================= 🚨 E R R O R 🚨 =========================");
     console.log(error);
+    console.log("\x1b[31m%s\x1b[0m", "=========================== 🚨 E N D 🚨 ===========================");
   }
 }
 
