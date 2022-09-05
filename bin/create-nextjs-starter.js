c8807c23fac630fa0b3b0be7b6c127a95a5e4a75
Yang Hong Seok <davidyang2149@gmail.com>
2022-09-05 11:28:54 +0900

    :recycle: 불필요한 코드를 정리합니다

--- bin/create-nextjs-starter.js
+++ bin/create-nextjs-starter.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-const fs = require('fs');
 const path = require('path');
 const { program } = require('commander');
-
-const { log } = console;
 
 const { setupFolder, setupPackage } = require('./validator');
 
