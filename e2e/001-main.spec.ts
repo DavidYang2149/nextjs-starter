592f7234d2d53788fa108ee60df4cc1da53e9e57
Davidyang2149 <davidyang2149@gmail.com>
2024-06-19 08:46:56 +0900

    🧪 #145 Test: dci 패턴을 이용하여 테스트 케이스를 추가하라

--- e2e/001-main.spec.ts
+++ e2e/001-main.spec.ts
@@ -1,9 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { describe, context, it } from 'playwright-dci-pattern';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000');
 });
 
-test('has title', async ({ page }) => {
-  await expect(page).toHaveTitle(/Nextjs starter kit/);
+
+describe('Main 페이지에서', () => {
+  context('처음 접속하면', () => {
+    it('메인 이미지가 보입니다', async ({ page }) => {
+      await expect(page.getByRole('img', { name: 'Next.js Logo' })).toBeVisible();
+    });
+  });
 });
